7a05f545132d598609ac47e77fbd75362ac6ee93
bgyordan <yordan.yordanov@csop-varna.bg>
2026-05-29 09:53:26 +0300

    Update page.tsx

--- app/(protected)/orders/page.tsx
+++ app/(protected)/orders/page.tsx
@@ -46,6 +46,8 @@ export default async function OrdersPage({
       title="Заповеди"
       data={(data || []) as Record<string, unknown>[]}
       userRole={userRole}
+      userId={user.id}
+      userEmail={profile.email}
       totalCount={count || 0}
       page={page}
       pageSize={PAGE_SIZE}
